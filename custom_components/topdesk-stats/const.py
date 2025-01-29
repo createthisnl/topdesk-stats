72ddada46df012fa70a2d712a90b19e41178a302
J-D <info@createthis.nl>
2025-01-29 10:23:30 +0000

    Set-up

---
+++ custom_components/topdesk-stats/const.py
@@ -0,0 +1,8 @@
+"""Constants for topdesk-stats."""
+
+from logging import Logger, getLogger
+
+LOGGER: Logger = getLogger(__package__)
+
+DOMAIN = "topdesk-stats"
+ATTRIBUTION = "Data provided by your own TOPdesk instance"
--- custom_components/topdesk-stats/switch.py
+++ custom_components/topdesk-stats/switch.py
@@ -1,4 +1,4 @@
-"""Switch platform for integration_blueprint."""
+"""Switch platform for topdesk-stats."""
 
 from __future__ import annotations
 
@@ -17,8 +17,8 @@ if TYPE_CHECKING:
 
 ENTITY_DESCRIPTIONS = (
     SwitchEntityDescription(
-        key="integration_blueprint",
-        name="Integration Switch",
+        key="topdesk-stats",
+        name="TOPdesk statistics Switch",
         icon="mdi:format-quote-close",
     ),
 )
@@ -40,7 +40,7 @@ async def async_setup_entry(
 
 
 class IntegrationBlueprintSwitch(IntegrationBlueprintEntity, SwitchEntity):
-    """integration_blueprint switch class."""
+    """topdesk-stats switch class."""
 
     def __init__(
         self,
